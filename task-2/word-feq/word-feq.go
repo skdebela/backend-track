9c73835adc948269921a1bc5a46aee47bf40c2ee
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2025-11-19 21:01:47 +0300

    feat(task-2): update word-feq to ignore punctuations

--- task-2/word-feq/word-feq.go
+++ task-2/word-feq/word-feq.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"strings"
+	"regexp"
 )
 
 func WordCount(s string) int {
     if s == "" {
         return 0
     }
-    return len(strings.Fields(s))
+
+	reg, _ := regexp.Compile(`[^\w\s]`)
+	text := reg.ReplaceAllString(s, "")
+
+    return len(strings.Fields(text))
 }
 
 func main() {
-	text := "This is a test for work count"
+	text := "This is a test for work count!"
 	feq := WordCount(text)
 	fmt.Println(feq)
 }

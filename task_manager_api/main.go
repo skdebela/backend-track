ab7e1bb3e0341ff64db632a620c3f5846fa41613
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2025-11-24 09:48:10 +0300

    ref(learn-gin): move logic to handlers

--- task_manager_api/main.go
+++ task_manager_api/main.go
@@ -22,81 +22,83 @@ var tasks = []Task{
 	{ID: "3", Title: "Task 3", Description: "Description 3", DueDate: time.Now().Add(24 * time.Hour), Status: "Completed"},
 }
 
+func getTasks(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
+}
+
+func getTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for _, val := range tasks {
+		if val.ID == id {
+			ctx.JSON(http.StatusOK, val)
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func removeTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for i, val := range tasks {
+		if val.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			ctx.JSON(http.StatusOK, gin.H{"message": "Task removed"})
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func addTask(ctx *gin.Context) {
+	var newTask Task
+
+	if err := ctx.ShouldBindJSON(&newTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	tasks = append(tasks, newTask)
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Task created"})
+}
+
+func updateTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var updatedTask Task
+
+	if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			if updatedTask.Title != "" {
+				tasks[i].Title = updatedTask.Title
+			}
+			if updatedTask.Description != "" {
+				tasks[i].Description = updatedTask.Description
+			}
+			ctx.JSON(http.StatusOK, gin.H{"message": "Task updated"})
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	router.GET("/tasks", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
-	})
-
-	router.GET("/tasks/:id", func(ctx *gin.Context) {
-		id := ctx.Param("id")
-		for _, task := range tasks {
-			if task.ID == id {
-				ctx.JSON(http.StatusOK, gin.H{"task": task})
-				return
-			}
-		}
-		ctx.JSON(http.StatusNotFound, gin.H{"message": "task not found"})
-	})
-
-	router.PUT("/tasks/:id", func(ctx *gin.Context) {
-		id := ctx.Param("id")
-
-		var updatedTask Task
-
-		if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"errror": err.Error()})
-			return
-		}
-
-		for i, task := range tasks {
-			if task.ID == id {
-				if updatedTask.Title != "" {
-					tasks[i].Title = updatedTask.Title
-				}
-				if updatedTask.Description != "" {
-					tasks[i].Description = updatedTask.Description
-				}
-				ctx.JSON(http.StatusOK, gin.H{"message": "Task updated"})
-				return
-			}
-		}
-
-		ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
-	})
-
-	router.DELETE("/tasks/:id", func(ctx *gin.Context) {
-		id := ctx.Param(("id"))
-
-		for i, val := range tasks {
-			if val.ID == id {
-				tasks = append(tasks[:i], tasks[i+1:]...)
-				ctx.JSON(http.StatusOK, gin.H{"message": "Task removed"})
-				return
-			}
-		}
-
-		ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
-	})
-
-	router.POST("/tasks/:id", func(ctx *gin.Context) {
-		var newTask Task
-
-		if err := ctx.ShouldBindJSON(&newTask); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		tasks = append(tasks, newTask)
-		ctx.JSON(http.StatusCreated, gin.H{"message": "Task creaed"})
-	})
+	router.GET("/tasks", getTasks)
+	router.GET("/tasks/:id", getTask)
+	router.DELETE("/tasks/:id", removeTask)
+	router.PUT("/tasks/:id", updateTask)
+	router.POST("/tasks", addTask)
 
 	err := router.Run(":8080")
 	if err != nil {
